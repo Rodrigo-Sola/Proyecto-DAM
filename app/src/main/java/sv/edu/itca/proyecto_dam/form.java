9a446b9dae12bfdb573b9082d0d5a1fcb43ed63d
RODRIGO-SOLA <rodrigo.claros24@itca.edu.sv>
2025-11-12 18:18:02 -0600

    Feat: Conexion entre activivitys y creacion de nueva activity de notificacionciones

--- app/src/main/java/sv/edu/itca/proyecto_dam/form.java
+++ app/src/main/java/sv/edu/itca/proyecto_dam/form.java
@@ -1,13 +1,51 @@
 package sv.edu.itca.proyecto_dam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class form extends AppCompatActivity {
+
+    private Button Guardar, Cancelar;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        Guardar = findViewById(R.id.btnGuardar);
+        Cancelar = findViewById(R.id.btnCancelar);
+
+        setonClickListener();
     }
+
+    private void setonClickListener()
+    {
+        Guardar.setOnClickListener(view ->  guarrarHabilidad());
+        Cancelar.setOnClickListener(view -> cancelarActivyti());
+
+
+    }
+
+    private void cancelarActivyti() {
+        Intent intent = new Intent(this, Home2Activity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void guarrarHabilidad() {
+
+        Intent intent = new Intent(this, Home2Activity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+
+
 }
